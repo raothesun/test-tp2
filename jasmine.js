10e8b2ffe94861e92eb6012412e37d9fa408a39c
samsepiol <samsepiol@samsepiol.samsepiol>
2018-02-07 20:55:11 +0100

    Jasmine & Postman edits

--- jasmine.js
+++ jasmine.js
@@ -1,3 +1,8 @@
+var fs = require('fs');
+fs.readdirSync('src/').forEach(function (file) {
+    eval(fs.readFileSync('src/' + file) + '');
+});
+
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
 
@@ -5,7 +10,7 @@ var jasmine = new Jasmine();
 jasmine.loadConfig({
     "spec_dir": "test/unit",
     "spec_files": [
-        "*.js"
+        "**/**.js"
     ],
     "stopSpecOnExpectationFailure": false,
     "random": false
